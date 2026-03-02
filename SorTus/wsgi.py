530d5af616a2e386542816a3973410a1e7ec770c
Prince Chavda <princechavada897@gmail.com>
2026-03-02 11:07:24 +0530

    Initial commit with Vercel configuration

---
+++ SorTus/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for SocialMedia project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SocialMedia.settings')
+
+application = get_wsgi_application()
+
+# Add this line so Vercel can find your app
+app = application
